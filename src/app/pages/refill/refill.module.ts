5dd5c644d6fc7f110f584f968dd699fc28106761
vadiz <vadizsolt@gmail.com>
2023-05-05 07:27:57 +0200

    csomagok hozzáadása

--- src/app/pages/refill/refill.module.ts
+++ src/app/pages/refill/refill.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RefillRoutingModule } from './refill-routing.module';
 import { RefillComponent } from './refill.component';
+import {FlexModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
   declarations: [
     RefillComponent
   ],
-  imports: [
-    CommonModule,
-    RefillRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        RefillRoutingModule,
+        FlexModule,
+        MatButtonModule,
+        MatCardModule
+    ]
 })
 export class RefillModule { }
--- src/app/pages/subscribe/subscribe.module.ts
+++ src/app/pages/subscribe/subscribe.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { SubscribeRoutingModule } from './subscribe-routing.module';
 import { SubscribeComponent } from './subscribe.component';
+import {MatCardModule} from "@angular/material/card";
+import {FlexModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { SubscribeComponent } from './subscribe.component';
   ],
   imports: [
     CommonModule,
-    SubscribeRoutingModule
+    SubscribeRoutingModule,
+    MatCardModule,
+    FlexModule,
+    MatButtonModule
   ]
 })
 export class SubscribeModule { }
